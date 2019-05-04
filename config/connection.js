3368ae44ddc9c2f817055787c8e45fbda12b23c1
tommyyxou <tommy.yx.ou@gmail.com>
2019-05-04 11:35:25 -0700

    test

--- config/connection.js
+++ config/connection.js
@@ -13,8 +13,8 @@ if (process.env.CLEARDB_DATABASE_URL) {
     connection = mysql.createConnection({
         host: 'us-cdbr-iron-east-02.cleardb.net',
         port: 3306,
-        user: b2c8985c6e27a5,
-        password: ec1799e7,
+        user: "b2c8985c6e27a5",
+        password: "ec1799e7",
         database: "heroku_d807133fc14176e"
         });
    // mysql://b2c8985c6e27a5:ec1799e7@us-cdbr-iron-east-02.cleardb.net/heroku_d807133fc14176e?reconnect=true
