c881fba3a8b07e874ac9a0477e6bf3d266292132
tommyyxou <tommy.yx.ou@gmail.com>
2019-05-03 23:00:12 -0700

    port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let path = require("path");
 // Sets up the Express App
 // =============================================================
 let app = express();
-let PORT = 3000;
+let PORT = 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
